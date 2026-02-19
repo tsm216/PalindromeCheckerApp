7a9ba86ba2b19d045075af35c55ce275cc391dcb
tsm216 <tm2437@srmist.edu.in>
2026-02-19 10:19:58 +0530

    Tarun Added UseCase2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -19,20 +19,14 @@
  * no palindrome logic is implemented uet
  *
  *the goal is to e
- *
- *
- *
- *
- *
- *
- *
- *
- * /
+ *git add App/src/PalindromeCheckerApp.java
+ **/
 
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
