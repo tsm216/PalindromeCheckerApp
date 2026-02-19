7763c90d957b1b4bc6eba2a2d9a0284a5170afa6
tsm216 <tm2437@srmist.edu.in>
2026-02-19 09:39:36 +0530

    Tarun Added UseCase1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//version 1.0
+//author tarun
+//usecase 1: welcome page
+
+/**
+ * MAIN CLASS - UseCase1PalindromeApp
+ * Use Case 1 : Application entry and welcome page
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System
+ *
+ * At this stage, the application:
+ * -starts execution fromt he main() method
+ * -dislays a welcome messages
+ * -shows application version
+ *
+ * no palindrome logic is implemented uet
+ *
+ *the goal is to e
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * /
+
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management System");
+    }
+}
